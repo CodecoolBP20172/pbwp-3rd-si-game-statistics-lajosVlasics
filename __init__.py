3119fb4ed111d856650f8fe4a4859939a7ceacd3
Lajos Balázs Vlasics <vlasicslajos@gmail.com>
2017-07-03 19:54:12 +0200

    add printing func

---
+++ __init__.py
@@ -0,0 +1,1 @@
+__all__ = ["count_games", "decide", "get_latest", "count_by_genre", "get_line_number_by_title", "sort_abc", "get_genres", "when_was_top_sold_fps"]
--- reports.py
+++ reports.py
@@ -10,16 +10,18 @@ def read_file(file_name):
 
 def read_to_dict(file_name):
     all_games = []
-    key_names = ("title", "copies", "release", "genre", "publisher")
-    read_data = read_file(file_name)
-    for row in read_data.split("\n"):
+    KEY_NAMES = ("title", "copies", "release", "genre", "publisher")
+    games_doc = read_file(file_name)
+    for row in games_doc.split("\n"):
         game_for_dict = {}
         i = 0
         for word in row.split("\t"):
-                game_for_dict[(key_names[i])] = word
+                game_for_dict[(KEY_NAMES[i])] = word
                 i += 1
         if game_for_dict["title"] == "":  # clean
             break
+        game_for_dict["copies"] = float(game_for_dict["copies"])
+        game_for_dict["release"] = int(game_for_dict["release"])
         all_games.append(game_for_dict)
     return all_games
 
@@ -30,17 +32,16 @@ def count_games(file_name):
 
 def decide(file_name, year):
     for game in read_to_dict(file_name):
-        if int(game["release"]) == year:
+        if game["release"] == year:
             return True
 
 
 def get_latest(file_name):
-    latest_game = ""
-    latest_release = 1900
+    latest_release = 0
     for game in read_to_dict(file_name):
-        if int(game["release"]) > latest_release:
+        if game["release"] > latest_release:
             latest_game = game["title"]
-            latest_release = int(game["release"])
+            latest_release = game["release"]
     return latest_game
 
 
@@ -63,7 +64,6 @@ def get_line_number_by_title(file_name, title):
 
 def sort_abc(file_name):
     titles = []
-    sorted_titles = []
     for game in read_to_dict(file_name):
         titles.append(game["title"])
     sorted_titles = quicksort(titles)
@@ -87,20 +87,9 @@ def get_genres(file_name):
 
 def when_was_top_sold_fps(file_name):
     sold_num = 0
-    release_year = 0
     for game in read_to_dict(file_name):
         if game["genre"] == "First-person shooter":
-            if float(game["copies"]) > sold_num:
-                sold_num = float(game["copies"])
-                release_year = int(game["release"])
+            if game["copies"] > sold_num:
+                sold_num = game["copies"]
+                release_year = game["release"]
     return release_year
-
-
-#print(count_games("game_stat.txt"))
-#decide("game_stat.txt", 2000)
-#get_latest("game_stat.txt")
-#print(count_by_genre("game_stat.txt", "First-person shooter"))
-#print(get_line_number_by_title("game_stat.txt", "Counter-Strike"))
-#print(sort_abc("game_stat.txt"))
-#print(get_genres("game_stat.txt"))
-#when_was_top_sold_fps("game_stat.txt")
