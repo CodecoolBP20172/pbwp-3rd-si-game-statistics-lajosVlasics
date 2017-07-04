f87a49c7ecd9220ed8e91bcfeec6c99719e8f9ca
Lajos Balázs Vlasics <vlasicslajos@gmail.com>
2017-07-04 10:52:12 +0200

    Final for Game statistics reports 1

--- export.py
+++ export.py
@@ -1,2 +1,58 @@
 
 # Export functions
+
+import time
+
+from reports import *
+
+
+def export_report(input_file, output_file, report, arg=None):
+    with open(output_file, "a") as f:
+        if arg is None:
+            f.write(str(report(input_file)) + "\n")
+        else:
+            f.write(str(report(input_file, arg)) + "\n")
+        f.close()
+
+
+def main():
+    while True:
+        reports_1arg = [count_games, get_latest, sort_abc, get_genres, when_was_top_sold_fps]
+        reports_2arg = [decide, count_by_genre, get_line_number_by_title]
+        report_list_all = [
+                        "this is a list of report functions",
+                        count_games,
+                        decide,
+                        get_latest,
+                        count_by_genre,
+                        get_line_number_by_title,
+                        sort_abc,
+                        get_genres,
+                        when_was_top_sold_fps]
+        print ("\033c")
+        print("Choose a report function by number!")
+        print("""
+        1: count_games
+        2: decide
+        3: get_latest
+        4: count_by_genre
+        5: get_line_number_by_title
+        6: sort_abc
+        7: get_genres
+        8: when_was_top_sold_fps\n""")
+
+        choosed_report = int(input())
+
+        if report_list_all[choosed_report] in reports_1arg:
+            export_report("game_stat.txt", "export.txt", report_list_all[choosed_report])
+            print("Export done!")
+            time.sleep(1)
+            
+        if report_list_all[choosed_report] in reports_2arg:
+            print("Give an argument too...")
+            user_arg = input()
+            export_report("game_stat.txt", "export.txt", report_list_all[choosed_report], user_arg)
+            print("Export done!")
+            time.sleep(1)
+
+main()
--- reports.py
+++ reports.py
@@ -1,6 +1,7 @@
 
 # Report functions
 
+
 def read_file(file_name):
     with open(file_name, "r") as f:
         read_data = f.read()
@@ -8,37 +9,36 @@ def read_file(file_name):
         return read_data
 
 
-def read_to_dict(file_name):
+def read_games(file_name):
     all_games = []
-    KEY_NAMES = ("title", "copies", "release", "genre", "publisher")
-    games_doc = read_file(file_name)
-    for row in games_doc.split("\n"):
-        game_for_dict = {}
+    for row in read_file(file_name).split("\n"):
+        game = {}
+        KEY_NAMES = ("title", "copies", "release", "genre", "publisher")
         i = 0
         for word in row.split("\t"):
-                game_for_dict[(KEY_NAMES[i])] = word
-                i += 1
-        if game_for_dict["title"] == "":  # clean
+            game[(KEY_NAMES[i])] = word
+            i += 1
+        if game["title"] == "":
             break
-        game_for_dict["copies"] = float(game_for_dict["copies"])
-        game_for_dict["release"] = int(game_for_dict["release"])
-        all_games.append(game_for_dict)
+        game["copies"] = float(game["copies"])
+        game["release"] = int(game["release"])
+        all_games.append(game)
     return all_games
 
 
 def count_games(file_name):
-    return len(read_to_dict(file_name))
+    return len(read_games(file_name))
 
 
 def decide(file_name, year):
-    for game in read_to_dict(file_name):
-        if game["release"] == year:
+    for game in read_games(file_name):
+        if game["release"] == int(year):
             return True
 
 
 def get_latest(file_name):
     latest_release = 0
-    for game in read_to_dict(file_name):
+    for game in read_games(file_name):
         if game["release"] > latest_release:
             latest_game = game["title"]
             latest_release = game["release"]
@@ -47,7 +47,7 @@ def get_latest(file_name):
 
 def count_by_genre(file_name, genre):
     genre_count = 0
-    for game in read_to_dict(file_name):
+    for game in read_games(file_name):
         if game["genre"] == genre:
             genre_count += 1
     return genre_count
@@ -55,7 +55,7 @@ def count_by_genre(file_name, genre):
 
 def get_line_number_by_title(file_name, title):
     line_count = 1
-    for game in read_to_dict(file_name):
+    for game in read_games(file_name):
         if game["title"] == title:
             return line_count
         line_count += 1
@@ -64,7 +64,7 @@ def get_line_number_by_title(file_name, title):
 
 def sort_abc(file_name):
     titles = []
-    for game in read_to_dict(file_name):
+    for game in read_games(file_name):
         titles.append(game["title"])
     sorted_titles = quicksort(titles)
     return sorted_titles
@@ -73,21 +73,21 @@ def sort_abc(file_name):
 def quicksort(lst):
     if not lst:
         return []
-    return (quicksort([x for x in lst[1:] if x <  lst[0]])
-            + [lst[0]] +
+    return (quicksort([x for x in lst[1:] if x < lst[0]]) +
+            [lst[0]] +
             quicksort([x for x in lst[1:] if x >= lst[0]]))
 
 
 def get_genres(file_name):
     genres_set = set()
-    for game in read_to_dict(file_name):
+    for game in read_games(file_name):
         genres_set.add(game["genre"])
     return sorted(list(genres_set), key=str.lower)
 
 
 def when_was_top_sold_fps(file_name):
     sold_num = 0
-    for game in read_to_dict(file_name):
+    for game in read_games(file_name):
         if game["genre"] == "First-person shooter":
             if game["copies"] > sold_num:
                 sold_num = game["copies"]
